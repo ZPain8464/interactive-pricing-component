7db04bb866edd60a0d802bdb3d09bf81053376b6
Zach Painter <z.painter45@gmail.com>
2021-03-23 14:45:02 -0500

    first commit

---
+++ js/functions.js
@@ -0,0 +1,70 @@
+function checkToggle() {
+  const element = document.getElementById("toggle");
+  element.classList.toggle("active");
+
+  // account for user hitting toggle multiple times on same price point
+  // if active, do math and git discounted total
+  // if else / back to inactive, render original amount
+}
+
+// sets pageviews based on scale value; passes value into
+// getDiscount() to calculate pricing
+function setPageViews() {
+  const r = document.getElementById("myRange");
+  const currentVal = r.value;
+  if (currentVal <= 17) {
+    const amount = 8;
+    const value = "10K PAGEVIEWS";
+    const renderPgViews = (document.getElementById(
+      "pageviews"
+    ).innerHTML = value);
+    getDiscount(amount);
+  } else if (currentVal <= 34) {
+    const amount = 12;
+    const value = "50K PAGEVIEWS";
+    const renderPgViews = (document.getElementById(
+      "pageviews"
+    ).innerHTML = value);
+    getDiscount(amount);
+  } else if (currentVal <= 50) {
+    const amount = 16;
+    const value = "100K PAGEVIEWS";
+    const renderPgViews = (document.getElementById(
+      "pageviews"
+    ).innerHTML = value);
+    getDiscount(amount);
+  } else if (currentVal <= 75) {
+    const amount = 24;
+    const value = "500K PAGEVIEWS";
+    const renderPgViews = (document.getElementById(
+      "pageviews"
+    ).innerHTML = value);
+    getDiscount(amount);
+  } else if (currentVal <= 100) {
+    const amount = 36;
+    const value = "1M PAGEVIEWS";
+    const renderPgViews = (document.getElementById(
+      "pageviews"
+    ).innerHTML = value);
+    getDiscount(amount);
+  }
+}
+
+function getDiscount(amount) {
+  const element = document.getElementById("toggle");
+  if (element.classList.contains("active")) {
+    const amountToSubtract = amount / 4;
+    const disAmount = amount - amountToSubtract;
+    const renderAmount = "$" + disAmount.toString() + `.00`;
+    const adjPricing = (document.getElementById(
+      "dollar-amount"
+    ).innerHTML = renderAmount);
+    return adjPricing;
+  } else {
+    const renderAmount = "$" + amount.toString() + `.00`;
+    const adjPricing = (document.getElementById(
+      "dollar-amount"
+    ).innerHTML = renderAmount);
+    return adjPricing;
+  }
+}
